3e310b261604d1835c37bfb4bf42228b43c2b2ec
Roman <roman.makhanbetov@gmx.de>
2022-07-27 10:52:50 +0200

    Interfaces

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,6 +4,11 @@ public class App {
     
     public static void main(String[] args) {
      
+
+        Bird bird = new Bird();
+        output(bird.fly());
+        output(bird.hasFeathers());
+
     }
 
     public static void output(String outputStr) {
